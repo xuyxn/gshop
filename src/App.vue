44d80b6ee8071d5dca36c87f71457c21317de636
xyx <xuyxjob@163.com>
2021-11-04 20:07:20 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <footer-guide v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,61 @@
+<template>
+  <header class="header">
+    <!-- 定义具名插槽 -->
+    <slot name="left"></slot>
+    <span class="header_title">
+      <!-- 中间这个部分是固定有的，接收一个属性title -->
+      <span class="header_title_text ellipsis">{{title}}</span>
+    </span>
+    <slot name="right"></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.header
+  background-color #02a774
+  position fixed
+  z-index 100
+  left 0
+  top 0
+  width 100%
+  height 45px
+  .header_search
+    position absolute
+    left 15px
+    top 50%
+    transform translateY(-50%)
+    width 10%
+    height 50%
+    .icon-sousuo
+      font-size 25px
+      color #fff
+  .header_title
+    position absolute
+    top 50%
+    left 50%
+    transform translate(-50%, -50%)
+    width 50%
+    color #fff
+    text-align center
+    .header_title_text
+      font-size 20px
+      color #fff
+      display block
+  .header_login
+    font-size 14px
+    color #fff
+    position absolute
+    right 15px
+    top 50%
+    transform translateY(-50%)
+    .header_login_text
+      color #fff
+</style>
